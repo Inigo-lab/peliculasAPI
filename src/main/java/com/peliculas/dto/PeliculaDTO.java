3ccacfc212aa555bd51e29b3990192d08b9388b3
Inigo-Lab <inigo.acosta@escuelaestech.es>
2024-09-11 02:04:04 +0200

    uploading and serving file

--- src/main/java/com/peliculas/dto/PeliculaDTO.java
+++ src/main/java/com/peliculas/dto/PeliculaDTO.java
@@ -1,43 +1,35 @@
-package com.peliculas.entities;
+package com.peliculas.dto;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
-@Entity
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-public class Pelicula {
+public class PeliculaDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer peliculaId;
 
-    @Column(nullable = false, length = 200)
     @NotBlank(message = "Por favor introduce un título para la película")
     private String titulo;
 
-    @Column(nullable = false)
     @NotBlank(message = "Por favor introduce un director para la película")
     private String director;
 
-    @Column(nullable = false)
     @NotBlank(message = "Por favor introduce un estudio para la película")
     private String estudio;
 
-    @ElementCollection
-    @CollectionTable(name = "reparto_pelicula")
     private Set<String> reparto;
 
-    @Column(nullable = false)
     private Integer anioLanzamiento;
 
-    @Column(nullable = false)
     @NotBlank(message = "Por favor introduce un poster para la película")
     private String poster;
+
+    @NotBlank(message = "Por favor introduce un poster para la película")
+    private String posterUrl;
 }
--- src/main/java/com/peliculas/repository/PeliculaRepository.java
+++ src/main/java/com/peliculas/repository/PeliculaRepository.java
@@ -2,6 +2,8 @@ package com.peliculas.repository;
 
 import com.peliculas.entities.Pelicula;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PeliculaRepository extends JpaRepository<Pelicula, Integer> {
 }
